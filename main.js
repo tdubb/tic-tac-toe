0043ecb1c9cc27de8676adfd7fb51eb066ff68e9
MakerSquare Student <students@themakersquare.com>
2013-11-22 17:02:25 -0600

    I have everything working except the name of who wins is wrong and it scores a win for the wrong winner

--- main.js
+++ main.js
@@ -9,6 +9,18 @@ var spaces = [
 var player1 = 'veggies';
 var player2 = 'junkfood';
 var currentPlayer = null;
+var winner = false;
+var veg = 0;
+var jun = 0;
+var increase = function () {
+  if (currentPlayer === player1)
+    return veg += 1;
+  else
+    return jun += 1;
+};
+
+$('#title').hide();
+
 
 var setNextTurn = function () {
   if (currentPlayer === player1) {
@@ -28,12 +40,47 @@ var checkForWinner = function () {
   if ( spaces[0] === spaces[1] && spaces[1] === spaces[2]
     || spaces[3] === spaces[4] && spaces[4] === spaces[5]
     || spaces[6] === spaces[7] && spaces[7] === spaces[8]
+    || spaces[0] === spaces[4] && spaces[4] === spaces[8]
+    || spaces[2] === spaces[4] && spaces[4] === spaces[6]
+    || spaces[0] === spaces[3] && spaces[3] === spaces[6]
+    || spaces[1] === spaces[4] && spaces[4] === spaces[7]
+    || spaces[2] === spaces[5] && spaces[5] === spaces[8]
+    
+
     // TODO: Check for rest of game winning cases
   )
   {
     console.log('somebody won');
-    // TODO: Trigger 'game-win' event with the winning player as the event data
+    $(document).trigger('game-win', currentPlayer);
+    winner = true;
+    spaces = [
+    NaN, NaN, NaN,
+    NaN, NaN, NaN,
+    NaN, NaN, NaN
+    ];
+    if (winner === true ) {
+      alert('Game Over');
+      $('#title').show();
+      $('#board .space').removeClass('veggies');
+      $('#board .space').removeClass('junkfood');
+      $('#board .space').addClass('truthy');
+      $('h3').hide(); 
+      increase();
+      $('#jun').text(jun);
+      $('#veg').text(veg);
+
+    }
+    $('button').css("display", '');
+    $('button').on("click", function () {
+      var i = 0;
+      $('#board .space').removeClass('truthy');
+      $('#title').hide();
+      $('h3').show();
+      // setNextTurn();
+    });
+    
   }
+
 };
 
 $(document).on('click', '#board .space', function (e) {
@@ -42,17 +89,27 @@ $(document).on('click', '#board .space', function (e) {
 
   // Marks the space with the current player's name
   // TODO: Don't mark it unless the space is blank
-  spaces[spaceNum] = currentPlayer;
+ 
+ 
+
   // Adds a class to elem so css can take care of the visuals
-  $('#board .space:eq(' + spaceNum + ')').addClass(currentPlayer);
+  
+  if (spaces[spaceNum]) {
+    alert('Choose another space');
+  }
+  else {
+    spaces[spaceNum] = currentPlayer;
+    $('#board .space:eq(' + spaceNum + ')').addClass(currentPlayer);
+    setNextTurn();  
+  }
 
   checkForWinner();
-  setNextTurn();
+  
 });
 
 $(document).on('game-win', function (e, winner) {
-  // TODO: Alert who won the game
+   alert(winner + " who won the game");
 });
 
 // Start the game
-setNextTurn();
+
